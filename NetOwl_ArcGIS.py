c040cdcbc354887243391091891cab52eb85b139
GeoJamesJones <32550234+GeoJamesJones@users.noreply.github.com>
2018-06-07 13:40:34 -0400

    Finished Toolbox

--- NetOwl_ArcGIS.py
+++ NetOwl_ArcGIS.py
@@ -2,6 +2,8 @@ import os
 import json
 import arcpy
 import requests
+import glob
+import zipfile
 import pandas as pd
 
 from rdflib import Graph
@@ -12,12 +14,14 @@ from arcgis.gis import GIS
 
 docsPath = arcpy.GetParameterAsText(0)
 rdfOutDir = arcpy.GetParameterAsText(1)
-rdfOutExt = arcpy.GetParameterAsText(2)
+outBaseName = arcpy.GetParameterAsText(2)
 fileOutDir = arcpy.GetParameterAsText(3)
 userName = arcpy.GetParameterAsText(4)
 passWord = arcpy.GetParameterAsText(5)
 
 gis = GIS(fileOutDir, username=userName, password=passWord)
+
+rdfOutExt = '.rdf'
 
 
 # Defines a function that will pass documents derived from the list
@@ -110,8 +114,9 @@ locations = gis.content.import_data(address, {"Address" : "label"})
 
 #Creates a hosted feature service from the feature
 # collection created in the previous step
+titleAdd  = outBaseName + "_Address"
 loc_properties = {
-    "title":"Address_NetOwl",
+    "title":titleAdd,
     "text": json.dumps({"featureCollection": {"layers": [dict(locations.layer)]}}),
     "type":"Feature Collection"}
 loc = gis.content.add(loc_properties)
@@ -136,9 +141,84 @@ locations = gis.content.import_data(address, {"Address" : "label"})
 
 #Creates a hosted feature service from the feature
 # collection created in the previous step
+titlePlace = outBaseName + "_Place"
 loc_properties = {
-    "title":"EarlyBird_Articles",
+    "title":titlePlace,
     "text": json.dumps({"featureCollection": {"layers": [dict(locations.layer)]}}),
     "type":"Feature Collection"}
 loc = gis.content.add(loc_properties)
 
+#Queries for the MGRS coordinates and writes them to a CSV file
+mgrs=e.select("""
+   SELECT ?s ?o ?label{
+      ?s netowl:Entity.Numeric.Coordinate.Mgrs..name ?o .
+      ?s rdfs:label ?label .
+    }
+""")
+
+mgrs.set_index("o")
+mgrs_file = os.path.join(rdfOutDir,'mgrs_coords.csv')
+mgrs.to_csv(mgrs_file, sep=',', encoding='utf-8')
+
+#Converts coordinates located in the MGRS CSV into Lat/Longs, turns this into a shapefile
+outShpDir = os.path.join(rdfOutDir, 'OutShp')
+outName = outBaseName + 'MGRS.shp'
+if os.path.exists(outShpDir) == False:
+    os.mkdir(outShpDir, mode=0o777,)
+arcpy.ConvertCoordinateNotation_management(in_table=mgrs_file, out_featureclass=os.path.join(outShpDir,outName), x_field="o", y_field="o", input_coordinate_format="MGRS", output_coordinate_format="DD_NUMERIC", id_field="",
+                                           spatial_reference="GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]];-400 -400 1000000000;-100000 10000;-100000 10000;8.98315284119522E-09;0.001;0.001;IsHighPrecision",
+                                           in_coor_system="GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]", exclude_invalid_records="INCLUDE_INVALID")
+
+#Zips up the shapefile created in the previous step
+outZip = fileOutDir
+
+def zipShapefilesInDir(inDir, outDir):
+    if not os.path.exists(inDir):
+        arcpy.AddMessage("Input directory %s does not exist!" % inDir)
+        return False
+
+    if not os.path.exists(outDir):
+        arcpy.AddMessage("Creating output directory %s" % outDir)
+        os.mkdir(outDir)
+
+    arcpy.AddMessage("Zipping shapefile(s) in folder %s to output folder %s" % (inDir, outDir))
+
+    for inShp in glob.glob(os.path.join(inDir, "*.shp")):
+        global outZip
+        outZip = os.path.join(outDir, os.path.splitext(os.path.basename(inShp))[0] + ".zip")
+
+        zipShapefile(inShp, outZip)
+    return True
+
+
+def zipShapefile(inShapefile, newZipFN):
+    arcpy.AddMessage('Starting to Zip ' + (inShapefile) + ' to ' + (newZipFN))
+
+    if not (os.path.exists(inShapefile)):
+        arcpy.AddMessage(inShapefile + ' Does Not Exist')
+        return False
+
+    if (os.path.exists(newZipFN)):
+        arcpy.AddMessage('Deleting ' + newZipFN)
+        os.remove(newZipFN)
+
+    if (os.path.exists(newZipFN)):
+        arcpy.AddMessage('Unable to Delete' + newZipFN)
+        return False
+
+    zipobj = zipfile.ZipFile(newZipFN, 'w')
+
+    for infile in glob.glob(inShapefile.lower().replace(".shp", ".*")):
+        if os.path.splitext(infile)[1].lower() != ".zip":
+            arcpy.AddMessage("Zipping %s" % (infile))
+            zipobj.write(infile, os.path.basename(infile), zipfile.ZIP_DEFLATED)
+
+    zipobj.close()
+    return True
+
+zipShapefilesInDir(outShpDir, rdfOutDir)
+
+#Uploads the shapefile to ArcGIS Online and publishes it as a feature service
+titleMGRS = outBaseName + '_MGRS'
+tempItem = gis.content.add({"title":titleMGRS, "type":"Shapefile"}, outZip)
+mgrsLyr = tempItem.publish()
